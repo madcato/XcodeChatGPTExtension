70e57ad236f4f89f0f19015b54b8a800fc4d45ec
Dani Vela <veladan@me.com>
2023-10-01 12:11:54 +0200

    initial commit

---
+++ XcodeChatGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeChatGPT
+//
+//  Created by Daniel Vela on 1/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeChatGPT/XcodeChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeChatGPTApp.swift
+//  XcodeChatGPT
+//
+//  Created by Daniel Vela on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct XcodeChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
